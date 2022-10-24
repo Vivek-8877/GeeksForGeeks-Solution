680a7f30860bf18d083341f0a16f7fbaaa9c232d
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-10-24 08:09:29 +0530

    Create Earthquake and the Paint Shop.cpp

---
+++ October 2022/Day 24/C++ Code/Earthquake and the Paint Shop.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+struct alphanumeric {
+    string name;
+    int count;
+};
+class Solution {
+  public:
+    vector<alphanumeric> sortedStrings(int N, vector<string> A) {
+        // code here
+        map<string,int> map;
+        for(string s : A) {
+            map[s]+=1;
+        }
+        
+        vector<alphanumeric> ans;
+        for (auto i = map.begin(); i!= map.end(); i++) {
+            struct alphanumeric a1;
+            a1.name = i->first;
+            a1.count = i->second;
+            ans.push_back(a1);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        cin.ignore();
+        vector<string> v;
+        for (int i = 0; i < N; i++) {
+            string s;
+            getline(cin, s);
+            v.push_back(s);
+        }
+        Solution ob;
+        vector<alphanumeric> ans = ob.sortedStrings(N, v);
+        for (auto u : ans) cout << u.name << " " << u.count << "\n";
+    }
+}
+// } Driver Code Ends
