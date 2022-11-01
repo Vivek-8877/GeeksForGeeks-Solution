52795688f6493c11f619edcbc1daa8a529c930ac
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-11-01 16:33:59 +0530

    Create Median in a row-wise sorted Matrix.cpp

---
+++ Novomber 2022/Day 1/C++ Code/Median in a row-wise sorted Matrix.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    
+    // Time Complexity :- O(32.r.log(c));
+    // Space Complexity :- O(1);
+    
+    int median(vector<vector<int>> &matrix, int R, int C){
+        // code here  
+        int lo = INT_MAX,hi=INT_MIN;
+        
+        for(int i=0;i<R;i++) {
+            lo = min(lo,matrix[i][0]);
+            hi = max(hi,matrix[i][C-1]);
+        }
+        int required_value=((R*C)/2);
+        if(R%2==0 || C%2==0) required_value--;
+        int ans=hi;
+        while(lo<=hi) {
+            int mid = lo+(hi-lo)/2;
+            
+            int count=0;
+            for(int i=0;i<R;i++) {
+                count+=search(matrix[i],mid);
+            }
+            
+            if(count<=required_value) {
+                lo=mid+1;
+            } else {
+                hi=mid-1;
+                ans=min(ans,mid);
+            }
+        }
+        return ans;
+    }
+    
+    int search(vector<int> a,int v) {
+        int lo=0,hi=a.size()-1;
+        int ans=-1;
+        while(lo<=hi) {
+            int mid = lo+(hi-lo)/2;
+            if(a[mid]<=v) {
+                lo=mid+1;
+                ans=max(ans,mid);
+            } else {
+                hi=mid-1;
+            }
+        }
+        return ans+1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int r, c;
+        cin>>r>>c;
+        vector<vector<int>> matrix(r, vector<int>(c));
+        for(int i = 0; i < r; ++i)
+            for(int j = 0;j < c; ++j)
+                cin>>matrix[i][j];
+        Solution obj;
+        cout<<obj.median(matrix, r, c)<<endl;        
+    }
+    return 0;
+}
+// } Driver Code Ends
