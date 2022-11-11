acf7215ea84ed517413b006c8807a772110e1c1a
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-11-11 06:51:58 +0530

    Create Primes sum.java

---
+++ Novomber 2022/Day 11/JAVA Code/Primes sum.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for Java
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+
+            System.out.println(ob.isSumOfTwo(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    // Time Complexity :- O(n.sqrt(n));
+    // Space Complexity :- O(1);
+    
+    static String isSumOfTwo(int N){
+        // code here
+        for(int i=2;i<=N/2;i++) {
+            if(isPrime(i) && isPrime(N-i)) {
+                return "Yes";
+            }
+        }
+        return "No";
+    }
+    
+    public static boolean isPrime(int n) {
+        for(int i=2;i*i<=n;i++) {
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+}
