b36f9065e13de373a79d936a02ad9f941d3c0af3
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-11-11 06:50:28 +0530

    Create Primes sum.cpp

---
+++ Novomber 2022/Day 11/C++ Code/Primes sum.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+public:
+    // Time Complexity :- O(n.sqrt(n));
+    // Space Complexity :- O(1);
+    
+    string isSumOfTwo(int N){
+        // code here
+        for(int i=2;i<=N/2;i++) {
+            if(isPrime(i) && isPrime(N-i)) {
+                return "Yes";
+            }
+        }
+        return "No";
+    }
+    bool isPrime(int n) {
+        for(int i=2;i*i<=n;i++) {
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.isSumOfTwo(N) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
