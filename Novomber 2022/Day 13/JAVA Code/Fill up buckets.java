4d42e737271c38a4036f6fbd39c59f1e6ae4a000
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-11-13 11:18:22 +0530

    Create Fill up buckets.java

---
+++ Novomber 2022/Day 13/JAVA Code/Fill up buckets.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine().trim();
+            String[] S = s.split(" ");
+            int[] capacity = new int[n];
+            for(int i = 0; i < n; i++)
+                capacity[i] = Integer.parseInt(S[i]);
+            Solution ob = new Solution();
+            int ans = ob.totalWays(n, capacity);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    // Time Complexity :- O(n.log(n));
+    // Space Complexity :- O(1);
+    
+    public int totalWays(int n, int[] capacity) {
+        // code here
+        Arrays.sort(capacity);
+        int ans = 1;
+        final int mod = 1000000007;
+        for(int i=0;i<n;i++) {
+            ans = (int)((ans*(long)(capacity[i]-i))%mod);
+        }
+        return ans;
+    }
+}
