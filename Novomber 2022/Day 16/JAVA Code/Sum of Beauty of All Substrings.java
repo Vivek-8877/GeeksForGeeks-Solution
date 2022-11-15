57956f6fbd612ca43bcbd704a3dd1ab078e8dabe
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-11-16 00:30:50 +0530

    Create Sum of Beauty of All Substrings.java

---
+++ Novomber 2022/Day 16/JAVA Code/Sum of Beauty of All Substrings.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            //br.readLine();
+            String s;
+            s = br.readLine();
+            
+            Solution obj = new Solution();
+            int res = obj.beautySum(s);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int beautySum(String s) {
+        // code here
+        int ans=0;
+        for(int i=0;i<s.length();i++) {
+            int[] fre = new int[26];
+            for(int j=i;j<s.length();j++) {
+                fre[s.charAt(j)-'a']++;
+                ans+=find(fre);
+            }
+        }
+        return ans;
+    }
+    
+    public static int find(int[] fre) {
+        int max=Integer.MIN_VALUE,min=Integer.MAX_VALUE;
+        for(int f : fre) {
+            if(f==0) continue;
+            max = Math.max(max,f);
+            min = Math.min(min,f);
+        }
+        return max-min;
+    }
+}
+        
