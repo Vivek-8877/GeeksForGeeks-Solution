819f947c496bbbf40ea398e2d8375f03d5062a4c
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-11-17 06:19:46 +0530

    Create Count of Subarrays.cpp

---
+++ Novomber 2022/Day 17/C++ Code/Count of Subarrays.cpp
@@ -0,0 +1,56 @@
+// Problem Link :- https://practice.geeksforgeeks.org/problems/count-of-subarrays5922/1
+// Video Solution Link :-
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// #define ll long long
+
+// 	Time Complexity :- O(n);
+// 	Space Complexity :- O(1);
+
+	ll countSubarray(int arr[], int n, int k) {
+	    // code here
+	    ll ans = (ll)n*(n+1)/2;
+        ll l =0;
+        for(int i=0;i<n;i++) {
+            if(arr[i]<=k) {
+                l++;
+            } else {
+                ans-=(l*(l+1)/2);
+                l=0;
+            }
+        }
+        ans-=(l*(l+1)/2);
+        return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k, i;
+        cin >> n >> k;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.countSubarray(arr, n, k);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
