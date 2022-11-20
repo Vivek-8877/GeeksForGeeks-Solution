e09c8069911eabde210ce8f397689664db9fcf1a
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-11-20 06:38:52 +0530

    Create LCM Triplet.cpp

---
+++ Novomber 2022/Day 20/C++ Code/LCM Triplet.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+// Initial Template for C++
+// Back-end complete function Template for C++
+// User function Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+//   Time Complexity :- O(1);
+//   Space Complexity :- O(1);
+  
+  public:
+    long long lcmTriplets(long long N) {
+        // code here
+        if(N<=2) return N;
+        if(N%2==0) {
+            if(N%3==0) {
+                return (N-1)*(N-2)*(N-3);
+            } else {
+                return (N-1)*(N-3)*(N);
+            }
+        } else {
+            return (N-2)*(N-1)*(N);
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        cin >> N;
+        Solution ob;
+        cout << ob.lcmTriplets(N) << "\n";
+    }
+}
+// } Driver Code Ends
