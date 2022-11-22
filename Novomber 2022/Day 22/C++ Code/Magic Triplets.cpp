acb596ddefe92f240808c9dc4c9736f726cdc24f
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-11-22 07:05:31 +0530

    Create Magic Triplets.cpp

---
+++ Novomber 2022/Day 22/C++ Code/Magic Triplets.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    // Time Complexity :- O(n^2);
+    // Space Complexity :- O(1);
+	
+	public:
+	int countTriplets(vector<int>nums){
+	    // Code here.
+	    int ans=0;
+	    for(int j=0;j<nums.size();j++) {
+	        int small =0;
+	        for(int i=0;i<j;i++) {
+	            if(nums[i]<nums[j]) small++;
+	        }
+	        
+	        int large =0;
+	        for(int k=j+1;k<nums.size();k++) {
+	            if(nums[j]<nums[k]) large++;
+	        }
+	        ans+=(small*large);
+	    }
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<int>nums(n);
+		for(int i = 0; i < n; i++)cin >> nums[i];
+		Solution ob;
+		int ans = ob.countTriplets(nums);
+		cout << ans << "\n";
+	}  
+	return 0;
+}
+// } Driver Code Ends
