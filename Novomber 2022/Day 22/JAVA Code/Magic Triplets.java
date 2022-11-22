ac69fa58cb578ea8c3ec67258a544789d4d1eee8
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-11-22 07:04:55 +0530

    Create Magic Triplets.java

---
+++ Novomber 2022/Day 22/JAVA Code/Magic Triplets.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine().trim();
+            String[] S = s.split(" ");
+            int[] nums = new int[n];
+            for(int i = 0; i < n; i++){
+                nums[i] = Integer.parseInt(S[i]);
+            }
+            Solution ob = new Solution();
+            int ans = ob.countTriplets(nums);
+            System.out.println(ans);         
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    // Time Complexity :- O(n^2);
+    // Space Complexity :- O(1);
+    
+    public int countTriplets(int[] nums){
+        // code here
+        int ans=0;
+	    for(int j=0;j<nums.length;j++) {
+	        int small =0;
+	        for(int i=0;i<j;i++) {
+	            if(nums[i]<nums[j]) small++;
+	        }
+	        
+	        int large =0;
+	        for(int k=j+1;k<nums.length;k++) {
+	            if(nums[j]<nums[k]) large++;
+	        }
+	        ans+=(small*large);
+	    }
+	    return ans;
+    }
+}
