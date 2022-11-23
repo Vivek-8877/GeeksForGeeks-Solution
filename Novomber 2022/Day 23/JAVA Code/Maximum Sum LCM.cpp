37e5b51b8d6eb16c6fcaf1138033202a0dfa2a59
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-11-23 07:14:56 +0530

    Create Maximum Sum LCM.cpp

---
+++ Novomber 2022/Day 23/JAVA Code/Maximum Sum LCM.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+
+            System.out.println(ob.maxSumLCM(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    // Time Complexity :- O(sqrt(n));
+    // Space Complexity :- O(1);
+    
+    static long maxSumLCM(int n) {
+        // code here
+        long ans=0;
+        for(long i=1;i*i<=n;i++) {
+            if(n%i==0) {
+                ans+=i;
+                if(i!=n/i) ans+=(n/i);
+            }
+        }
+        return ans;
+    }
+}
