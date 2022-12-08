1e2865be905a0c47946ab705619684dd4d160259
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-12-08 08:36:29 +0530

    Create 3 Divisors.cpp

---
+++ December 2022/Day 8/C++ Code/3 Divisors.cpp
@@ -0,0 +1,81 @@
+// Problem Link :- https://practice.geeksforgeeks.org/problems/3-divisors3942/1
+// Video Solution Link :- https://www.youtube.com/channel/UC1P9bWDLTPS1Zp03m5PQEoQ
+
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    // Time Complexity :- O(q*N*log(log(N)));
+    // Space Complexity :- O(N), where N is min(10^6,N);
+    vector<int> threeDivisors(vector<long long> query, int q)
+    {
+        // Write your code here
+        vector<int> prime = primeNumber(1000000);
+        vector<int> ans;
+        for(long q1 : query) {
+            ans.push_back(solve(prime,q1));
+        }
+        return ans;
+    }
+    
+    vector<int> primeNumber(int n) {
+        vector<int> prime(n+1,1);
+        prime[0]=0;
+        prime[1]=0;
+        
+        for(int i=2;i<=n;i++) {
+            if(prime[i]==1) {
+                for(int j=i+i;j<=n;j+=i) {
+                    prime[j]=0;
+                }
+            }
+        }
+        vector<int> list;
+        for(int i=0;i<=n;i++) {
+            if(prime[i]==1) list.push_back(i);
+        }
+        return list;
+    }
+    
+    int solve(vector<int> prime,long long q) {
+        int v1 = (int)sqrt(q);
+        int lo=0,hi=prime.size()-1;
+        int ans=0;
+        while(lo<=hi) {
+            int mid = lo+(hi-lo)/2;
+            if(prime[mid]<=v1) {
+                lo=mid+1;
+                ans = max(ans,mid+1);
+            } else {
+                hi=mid-1;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--){
+        int q;cin>>q;
+        vector<long long> query(q);
+        for(int i=0;i<q;i++){
+            cin>>query[i];
+        }
+        Solution ob;
+            
+        vector<int> ans = ob.threeDivisors(query,q);
+        for(auto cnt : ans) {
+            cout<< cnt << endl;
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
