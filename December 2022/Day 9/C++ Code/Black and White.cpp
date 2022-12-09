7fbccd8e3d7e00c708a25dc3322272c7da7ee92e
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-12-09 06:06:36 +0530

    Create Black and White.cpp

---
+++ December 2022/Day 9/C++ Code/Black and White.cpp
@@ -0,0 +1,78 @@
+// Problem Link :- https://practice.geeksforgeeks.org/problems/black-and-white-1587115620/1
+// Video Solution Link :- https://www.youtube.com/channel/UC1P9bWDLTPS1Zp03m5PQEoQ
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long numOfWays(int n, int m);
+
+int main() {
+    // code
+    int T;
+    cin >> T;
+    while (T--) {
+        int n, m;
+        cin >> n >> m;
+        cout << numOfWays(n, m) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+//Function to find out the number of ways we can place a black and a 
+//white Knight on this chessboard such that they cannot attack each other.
+int requiredMove(int i,int j,int n,int m) {
+    int ans=0;
+    if(i-1>0) {
+        if(j-2>0) {
+            ans++;
+        }
+        if(j+2<=m) {
+            ans++;
+        }
+    }
+    if(i-2>0) {
+        if(j-1>0) {
+            ans++;
+        }
+        if(j+1<=m) {
+            ans++;
+        }
+    }
+    if(i+1<=n) {
+        if(j-2>0) {
+            ans++;
+        }
+        if(j+2<=m) {
+            ans++;
+        }
+    }
+    if(i+2<=n) {
+        if(j-1>0) {
+            ans++;
+        }
+        if(j+1<=m) {
+            ans++;
+        }
+    }
+    return ans;
+}
+    // Time Complexity :- O(m*n);
+    // Space Complexity :- O(1);
+long long numOfWays(int N, int M)
+{
+    // write code here
+    int mod = 1000000007;
+    int ans =0;
+    int totalPosition = N*M;
+    for(int i=1;i<=N;i++) {
+        for(int j=1;j<=M;j++) {
+            ans+=((totalPosition-1)-requiredMove(i,j,N,M));
+            ans%=mod;
+        }
+    }
+    return ans;
+}
