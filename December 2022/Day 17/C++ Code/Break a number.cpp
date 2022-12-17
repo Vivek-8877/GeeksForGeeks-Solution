1b816d201f2c36c624dd211b3f1a46f84f9808cd
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-12-17 08:16:28 +0530

    Create Break a number.cpp

---
+++ December 2022/Day 17/C++ Code/Break a number.cpp
@@ -0,0 +1,38 @@
+// Problem Link :- https://practice.geeksforgeeks.org/problems/break-a-number5913/1
+// Video Solution Link :- https://www.youtube.com/channel/UC1P9bWDLTPS1Zp0
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{   
+public:
+    // Time Complexity :- O(1);
+    // Space Complexity :- O(1);
+    int waysToBreakNumber(int N){
+        // code here
+        long long int mod = 1000000007;
+        long long int n =N;
+        return (3*n+((n-1)*(n-2))/2)%mod;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.waysToBreakNumber(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
