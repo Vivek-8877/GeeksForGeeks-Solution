ff484fe86c74c584a8f8953f834230e8330d4420
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-12-17 08:15:38 +0530

    Create Break a number.java

---
+++ December 2022/Day 17/JAVA Code/Break a number.java
@@ -0,0 +1,37 @@
+// Problem Link :- https://practice.geeksforgeeks.org/problems/break-a-number5913/1
+// Video Solution Link :- https://www.youtube.com/channel/UC1P9bWDLTPS1Zp0
+
+//{ Driver Code Starts
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.waysToBreakNumber(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution{
+    // Time Complexity :- O(1);
+    // Space Complexity :- O(1);
+    private static final long mod = 1000000007;
+    int waysToBreakNumber(int N){
+        // code here
+        long n = N;
+        return (int)((3*n+((n-1)*(n-2))/2)%mod);
+    }
+}
