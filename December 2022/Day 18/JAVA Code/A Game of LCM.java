e35c3ad5eb854fce3d14053c345cff01a38ea835
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-12-18 13:05:52 +0530

    Create A Game of LCM.java

---
+++ December 2022/Day 18/JAVA Code/A Game of LCM.java
@@ -0,0 +1,53 @@
+// Problem Link :- https://practice.geeksforgeeks.org/problems/a-game-of-lcm2531/1
+// Video Solution Link :- 
+
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.maxGcd(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Time Complexity :- O(n);
+    // Space Complexity :- O(1);
+    long solve(long n) {
+        long a1 = n*(n-1);
+        int include=0;
+        for(long i=n-2;i>=1;i--) {
+            long g = gcd(a1,i);
+            if(g==1) {
+                a1*=i;
+                include++;
+            }
+            if(include==2) return a1;
+        }
+        return a1;
+    }
+    
+    long maxGcd(int N) {
+        // code here
+        return Math.max(solve(N),solve(N-1));
+    }
+    
+    long gcd(long a,long b) {
+        if(a==0) return b;
+        return gcd(b%a,a);
+    }
+}
