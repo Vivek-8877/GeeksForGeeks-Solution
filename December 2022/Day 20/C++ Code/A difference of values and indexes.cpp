23c31bca867d5c87343c52cad8a152f204f71ca1
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-12-20 15:33:06 +0530

    Create A difference of values and indexes.cpp

---
+++ December 2022/Day 20/C++ Code/A difference of values and indexes.cpp
@@ -0,0 +1,52 @@
+// Problem Link :- https://practice.geeksforgeeks.org/problems/a-difference-of-values-and-indexes0302/1
+// Video Solution Link :- 
+
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+    // Time Complexity :- O(n);
+    // Space Complexity :- O(1);
+    int maxDistance(int arr[], int n) 
+    { 
+        // Complete the function
+        int ans = INT_MIN;
+        int mx = INT_MIN;
+        int mn = INT_MAX;
+        
+        for(int i=0;i<n;i++) {
+            mx = max(mx,arr[i]-i);
+            mn = min(mn,arr[i]+i);
+            
+            int v1 = (arr[i]+i)-mn;
+            int v2 = mx-(arr[i]-i);
+            
+            ans = max(ans,max(v1,v2));
+        }
+        
+        return ans;
+    } 
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+	int T;
+	cin>>T;
+	while(T--){
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i = 0; i<n; i++)
+	        cin>>arr[i];
+	    Solution obj;
+	    cout<<obj.maxDistance(arr, n)<<endl;
+	}
+} 
+
+// } Driver Code Ends
