afea4c7876985a809d40829e7795c718f913310e
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-12-28 08:05:46 +0530

    Create Largest subtree sum in a tree.java

---
+++ December 2022/Day 28/JAVA Code/Largest subtree sum in a tree.java
@@ -0,0 +1,156 @@
+// Problem Link :- https://practice.geeksforgeeks.org/problems/largest-subtree-sum-in-a-tree/1
+// Video Solution Link :- 
+
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+    
+    public static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length()==0 || str.charAt(0)=='N')
+            return null;
+        
+        // Creating array of Strings from input 
+        // String after spliting by space
+        String ip[] = str.split(" ");
+        
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        
+        // Push the root to the queue
+        Queue<Node> queue = new LinkedList<>(); 
+        queue.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(queue.size()>0 && i < ip.length)
+        {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N"))
+            {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N"))
+            {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+    public static Node inputTree(BufferedReader br) throws IOException
+    {
+        return buildTree(br.readLine().trim());
+    }
+    
+    public static void inorder(Node root)
+    {
+        if (root == null)
+           return;
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            Node root = Node.inputTree(br);
+            
+            Solution obj = new Solution();
+            int res = obj.findLargestSubtreeSum(root);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+Definition for Binary Tree Node
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    // Time Complexity :- O(n);
+    // Space Complexity :- O(n);
+    public static int findLargestSubtreeSum(Node root) {
+        // code here
+        return f(root)[1];
+    }
+    // 0th index-> Sum of all node of tree rooted at root.
+    // 1st index :- Maximum sum possible till root node.
+    public static int[] f(Node root) {
+        if(root==null) return new int[]{0,Integer.MIN_VALUE};
+        int[] v1 = f(root.left);
+        int[] v2 = f(root.right);
+        int max = Math.max(v1[0]+v2[0]+root.data,Math.max(v1[1],v2[1]));
+        int sum = v1[0]+v2[0]+root.data;
+        return new int[]{sum,max};
+    }
+}
+        
