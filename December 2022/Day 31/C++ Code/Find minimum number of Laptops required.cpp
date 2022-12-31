7bf4ea9f93e540de5b77bd8f3adaa9985c8b884a
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-12-31 13:16:10 +0530

    Update Find minimum number of Laptops required.cpp

--- December 2022/Day 31/C++ Code/Find minimum number of Laptops required.cpp
+++ December 2022/Day 31/C++ Code/Find minimum number of Laptops required.cpp
@@ -1,5 +1,5 @@
 // Problem Link :- https://practice.geeksforgeeks.org/problems/af49b143a4ead583e943ca6176fbd7ea55b121ae/1
-// Video Solution Link :- https://youtu.be/HZkFMpu4EJs
+// Video Solution Link :- https://youtu.be/bHZontqMkfE
 
 //{ Driver Code Starts
 // Initial Template for C++
