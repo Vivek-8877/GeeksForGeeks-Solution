b16a4c86ddbc404f0cf9e3f3fc3727f3e0b0263d
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2023-01-05 07:51:28 +0530

    Added Solution - GfG to GitHub

---
+++ January 2023/Day 05/Find the longest string/find-the-longest-string.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+public:
+    string longestString(vector<string> &words)
+    {
+        // code here
+        unordered_set <string> set;
+        for(string word : words) {
+            set.insert(word);
+        }
+        string ans ="";
+        bool isValid;
+        for(string word : words) {
+            isValid=true;
+            for(int l=1;l<=word.length() && isValid;l++) {
+                if(set.find(word.substr(0,l))==set.end()) {
+                    isValid=false;
+                }
+            }
+            if(isValid) {
+                if(ans.length()<word.length()) {
+                    ans=word;
+                } else if(ans.length()==word.length()) {
+                    if(ans>word) ans=word;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<string> v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        Solution obj;
+        string w = obj.longestString(v);
+        cout << w << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
