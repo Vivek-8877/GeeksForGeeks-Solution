d577595199c90f447c8f48e2adc7bbe574f05d1f
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2023-01-12 06:30:31 +0530

    Added Solution - GfG to GitHub

---
+++ January 2023/Day 12/Minimize the sum/minimize-the-sum.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution {
+    long minimizeSum(int N, int arr[]) {
+        // code here
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int a : arr) pq.offer(a);
+        long ans=0,sum=0;
+        while(pq.size()>1) {
+            sum = pq.poll()+pq.poll();
+            pq.offer((int)sum);
+            ans+=sum;
+        }
+        return ans;
+    }
+}
+
+//{ Driver Code Starts.
+
+public class GFG
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while (t-- > 0)
+        {
+            int n = sc.nextInt();
+            int A[] = new int[n];
+            
+            for (int i = 0; i < n;i++)
+            {
+                A[i] = sc.nextInt();
+            }
+            Solution obj = new Solution();
+            long ans = obj.minimizeSum(n, A);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
