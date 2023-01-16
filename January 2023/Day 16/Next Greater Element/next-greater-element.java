4dddf9ae32598d8207b717d8ea1aae03546aebb6
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2023-01-16 06:50:31 +0530

    Added Solution - GfG to GitHub

---
+++ January 2023/Day 16/Next Greater Element/next-greater-element.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    int n = Integer.parseInt(br.readLine().trim());
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    long[] arr = new long[n];
+		    for(int i=0; i<n; i++)arr[i]=Long.parseLong(inputLine[i]);
+		    Solution ob = new Solution();
+		    long[] res = ob.nextLargerElement(arr, n);
+		    for (int i = 0; i < n; i++) 
+		        System.out.print(res[i] + " ");
+		    System.out.println();
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+// User Function Template for JAVA
+
+class Solution{
+    Time Complexity :- O(n)
+    public static long[] nextLargerElement(long[] arr, int n) { 
+        // Your code here
+        long[] ans = new long[n];
+        Stack<Long> stack = new Stack<>();
+        for(int i=n-1;i>=0;i--) {
+            while(stack.size()>0 && stack.peek()<arr[i]) {
+                stack.pop();
+            }
+            if(stack.size()==0) {
+                ans[i]=-1;
+            } else {
+                ans[i]=stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+        return ans;
+    } 
+}
