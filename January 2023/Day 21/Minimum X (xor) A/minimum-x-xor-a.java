237f544b865fa2d21aa13181614dca5334ff0a1f
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2023-01-21 10:13:57 +0530

    Updated Solution - GfG to GitHub

--- January 2023/Day 21/Minimum X (xor) A/minimum-x-xor-a.java
+++ January 2023/Day 21/Minimum X (xor) A/minimum-x-xor-a.java
@@ -29,39 +29,45 @@ class GFG {
 // User function Template for Java
 
 class Solution {
+    // Time Complexity :- O(max(log(a),log(b)));
+    // Space Complexity :- O(1);
     public static int minVal(int a, int b) {
         // code here
-        byte[] a1 = decimalToBinary(a);
-        byte[] b1 = decimalToBinary(b);
-        
+        int a1 = countSetBit(a);
+        int b1 = countSetBit(b);
         int ans = a;
-        if(a1[32]==b1[32]) {
+        if(a1==b1) {
             
-        } else if(a1[32]<b1[32]) {
-            for(int i=0;i<32 && a1[32]!=b1[32];i++) {
-                if(a1[i]==0) {
-                    ans+=(1<<i);
-                    a1[32]++;
+        } else if(a1<b1) {
+            int idx=0;
+            while(a1!=b1) {
+                if(a%2==0) {
+                    a1++;
+                    ans+=(1<<idx);
                 }
+                idx++;
+                a/=2;
             }
         } else {
-            for(int i=0;i<32 && a1[32]!=b1[32];i++) {
-                if(a1[i]==1) {
-                    ans-=(1<<i);
-                    a1[32]--;
+            int idx=0;
+            while(a1!=b1) {
+                if(a%2==1) {
+                    a1--;
+                    ans-=(1<<idx);
                 }
+                idx++;
+                a/=2;
             }
         }
         return ans;
     }
-    public static byte[] decimalToBinary(int n) {
-        byte[] binary = new byte[33];
-        for(int i=0;i<32;i++) {
-            if((n&(1<<i))!=0) {
-                binary[32]++;
-                binary[i]=1;
-            }
+    
+    public static int countSetBit(int n) {
+        int ct=0;
+        while(n>0) {
+            if(n%2==1) ct++;
+            n/=2;
         }
-        return binary;
+        return ct;
     }
 }
