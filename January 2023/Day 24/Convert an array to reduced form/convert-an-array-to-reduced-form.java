6910933e5d59acee8fac65d1fa403b14ff988882
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2023-01-24 07:38:18 +0530

    Added Solution - GfG to GitHub

---
+++ January 2023/Day 24/Convert an array to reduced form/convert-an-array-to-reduced-form.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().convert(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    void convert(int[] arr, int n) {
+        //Code here
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        for(int i=0;i<n;i++) {
+            map.put(arr[i],i);
+        }
+        
+        int idx=0;
+        for(int key : map.keySet()) {
+            arr[map.get(key)]=idx;
+            idx++;
+        }
+    }
+}
