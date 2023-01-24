4468644d77cd78d5a14b7c7792ea59156a4ed877
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2023-01-24 07:41:54 +0530

    Added Solution - GfG to GitHub

---
+++ January 2023/Day 24/Convert an array to reduced form/convert-an-array-to-reduced-form.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Converts arr[0..n-1] to reduced form.
+	void convert(int arr[], int n) {
+	    // code here
+	    map<int,int> mp;
+        for(int i=0;i<n;i++) {
+            mp[arr[i]]=i;
+        }
+        
+        int idx=0;
+        for(auto i=mp.begin();i!=mp.end();i++) {
+            arr[i->second]=idx;
+            idx++;
+        }
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.convert(arr, n);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
