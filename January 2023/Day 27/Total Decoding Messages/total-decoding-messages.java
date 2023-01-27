43d96ebbaee9839605793d85cb16f12755f887d0
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2023-01-27 10:01:11 +0530

    Updated Solution - GfG to GitHub

--- January 2023/Day 27/Total Decoding Messages/total-decoding-messages.java
+++ January 2023/Day 27/Total Decoding Messages/total-decoding-messages.java
@@ -29,6 +29,8 @@ class GFG
 
 class Solution
 {
+    // Time Complexity :- O(n);
+    // Space Complexity :- O(28*n) => O(n);
     public static final int mod = 1000000007;
     public int CountWays(String str)
     {
@@ -36,17 +38,17 @@ class Solution
         return f(str,0,-1,new Integer[str.length()][28]);
     }
     
-    public static int f(String s,int idx,int val,Integer[][] dp) {
-        if(val==0 || val>26) return 0;
+    public static int f(String s,int idx,int code,Integer[][] dp) {
+        if(code==0 || code>26) return 0;
         if(idx>=s.length()) {
             return 1;
         }
-        if(dp[idx][val+1]!=null) return dp[idx][val+1];
+        if(dp[idx][code+1]!=null) return dp[idx][code+1];
         int ans=0;
         ans = add(ans,f(s,idx+1,s.charAt(idx)-'0',dp));
-        if(idx!=0) ans = add(ans,f(s,idx+1,(10*val)+(s.charAt(idx)-'0'),dp));
+        if(idx!=0) ans = add(ans,f(s,idx+1,(10*code)+(s.charAt(idx)-'0'),dp));
         
-        return dp[idx][val+1]=ans;
+        return dp[idx][code+1]=ans;
     }
     
     public static int add(int a,int b) {
