57fdfae37c05f8bda1e3e3b59687d5686a854f50
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2023-01-28 14:43:46 +0530

    Updated Solution - GfG to GitHub

--- January 2023/Day 28/Scrambled String/scrambled-string.java
+++ January 2023/Day 28/Scrambled String/scrambled-string.java
@@ -35,29 +35,31 @@ class Geeks
 //User function Template for Java
 
 class Solution{
+    // Time Complexity :- O(n^4);
+    // Space Complexity :- O(n^4);
     static boolean isScramble(String S1,String S2)
     {
         //code here
         int n = S1.length();
-        return isValid(S1,S2,0,S1.length()-1,0,S2.length()-1,new Boolean[n][n][n][n]);
+        return isValid(S1,S2,0,n-1,0,n-1,new Boolean[n][n][n][n]);
     }
     
     static boolean isValid(String s1,String s2,int lo1,int hi1,int lo2,int hi2,Boolean[][][][] dp) {
-        // System.out.println(lo1+" "+hi1+" "+lo2+" "+hi2+" "+step);
         if(s1.substring(lo1,hi1+1).compareTo(s2.substring(lo2,hi2+1))==0) return true;
         
         if(dp[lo1][hi1][lo2][hi2]!=null) return dp[lo1][hi1][lo2][hi2];
+        
         boolean ans=true;
         for(int i=lo1,j=hi2;i<=hi1 && ans;i++,j--) {
-            // System.out.println(i+" "+j);
             if(s1.charAt(i)!=s2.charAt(j)) ans=false;
         }
         
         for(int i=0;i<hi1-lo1 && !ans;i++) {
-            if(isValid(s1,s2,lo1,lo1+i,lo2,lo2+i,dp) && isValid(s1,s2,lo1+i+1,hi1,lo2+i+1,hi2,dp)) ans=true;
-            if(!ans && isValid(s1,s2,lo1,lo1+i,hi2-i,hi2,dp) && isValid(s1,s2,lo1+i+1,hi1,lo2,hi2-i-1,dp)) ans=true;
+            if(isValid(s1,s2,lo1,lo1+i,lo2,lo2+i,dp) && 
+                isValid(s1,s2,lo1+i+1,hi1,lo2+i+1,hi2,dp)) ans=true;
+            if(!ans && isValid(s1,s2,lo1,lo1+i,hi2-i,hi2,dp) && 
+                isValid(s1,s2,lo1+i+1,hi1,lo2,hi2-i-1,dp)) ans=true;
         }
-        // System.out.println(step);
         return dp[lo1][hi1][lo2][hi2]=ans;
     }
     
