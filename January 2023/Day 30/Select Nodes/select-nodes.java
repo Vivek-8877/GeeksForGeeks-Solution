29986bddbcbb3bd9a895ec38f4d2e847df516178
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2023-01-30 10:00:45 +0530

    Updated Solution - GfG to GitHub

--- January 2023/Day 30/Select Nodes/select-nodes.java
+++ January 2023/Day 30/Select Nodes/select-nodes.java
@@ -33,6 +33,8 @@ class GFG
 //User function Template for Java
 
 class Solution{
+    // Time Complexity :- O(n);
+    // Space Complexity :- O(n);
     public int countVertex(int N, int[][] edges){
         // code here
         ArrayList<Integer>[] graph = new ArrayList[N+1];
@@ -41,26 +43,24 @@ class Solution{
             graph[e[0]].add(e[1]);
             graph[e[1]].add(e[0]);
         }
-        // for(int i=1;i<=N;i++) System.out.println(graph[i]);
-        return Math.min(f(graph,1,new byte[N+1],0,-1,new Integer[N+1][2]),f(graph,1,new byte[N+1],1,-1,new Integer[N+1][2])+1);
+        return Math.min(f(graph,1,0,-1,new Integer[N+1][2]),f(graph,1,1,-1,new Integer[N+1][2])+1);
     }
     
-    public static int f(ArrayList<Integer>[] graph,int src,byte[] visit,int isLighted,int prnt,Integer[][] dp) {
+    public static int f(ArrayList<Integer>[] graph,int src,int isLighted,int prnt,Integer[][] dp) {
         if(dp[src][isLighted]!=null) return dp[src][isLighted];
         int ans=0;
         for(int nbr : graph[src]) {
             if(nbr!=prnt) {
                 if(isLighted==0) {
-                    int f1 = f(graph,nbr,visit,1,src,dp);
+                    int f1 = f(graph,nbr,1,src,dp);
                     ans+=f1+1;
                 } else {
-                    int f0 = f(graph,nbr,visit,0,src,dp);
-                    int f1 = f(graph,nbr,visit,1,src,dp);
+                    int f0 = f(graph,nbr,0,src,dp);
+                    int f1 = f(graph,nbr,1,src,dp);
                     ans+=Math.min(f0,f1+1);
                 }
             }
         }
-        // System.out.println(src+" "+ans);
         return dp[src][isLighted]=ans;
     }
 }
