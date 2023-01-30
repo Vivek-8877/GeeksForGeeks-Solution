a7fce7fb701562e47d5fe5e7948cbe6c70a48af0
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2023-01-30 08:05:37 +0530

    Added Solution - GfG to GitHub

---
+++ January 2023/Day 30/Select Nodes/select-nodes.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            int[][]edge=new int[N-1][2];
+            for(int i=0;i<N-1;i++)
+            {
+                String input[] = read.readLine().trim().split("\\s+");
+                edge[i][0]=Integer.parseInt(input[0]);
+                edge[i][1]=Integer.parseInt(input[1]);
+            }
+            Solution ob = new Solution();
+            out.println(ob.countVertex(N, edge));
+        }
+        out.close();
+    }
+}
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution{
+    public int countVertex(int N, int[][] edges){
+        // code here
+        ArrayList<Integer>[] graph = new ArrayList[N+1];
+        for(int i=1;i<=N;i++) graph[i] = new ArrayList<>();
+        for(int[] e : edges) {
+            graph[e[0]].add(e[1]);
+            graph[e[1]].add(e[0]);
+        }
+        // for(int i=1;i<=N;i++) System.out.println(graph[i]);
+        return Math.min(f(graph,1,new byte[N+1],0,-1,new Integer[N+1][2]),f(graph,1,new byte[N+1],1,-1,new Integer[N+1][2])+1);
+    }
+    
+    public static int f(ArrayList<Integer>[] graph,int src,byte[] visit,int isLighted,int prnt,Integer[][] dp) {
+        if(dp[src][isLighted]!=null) return dp[src][isLighted];
+        int ans=0;
+        for(int nbr : graph[src]) {
+            if(nbr!=prnt) {
+                if(isLighted==0) {
+                    int f1 = f(graph,nbr,visit,1,src,dp);
+                    ans+=f1+1;
+                } else {
+                    int f0 = f(graph,nbr,visit,0,src,dp);
+                    int f1 = f(graph,nbr,visit,1,src,dp);
+                    ans+=Math.min(f0,f1+1);
+                }
+            }
+        }
+        // System.out.println(src+" "+ans);
+        return dp[src][isLighted]=ans;
+    }
+}
+
+//{ Driver Code Starts.
+
+// } Driver Code Ends
