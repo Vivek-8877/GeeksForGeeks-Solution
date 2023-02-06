bae4611293f8f2d928b29ad5e12d068fc4a31858
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2023-02-06 17:14:26 +0530

    Added Solution - GfG to GitHub

---
+++ February 2023/Day 06/BST Downward Traversal/bst-downward-traversal.java
@@ -0,0 +1,146 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        PrintWriter out=new PrintWriter(System.out);
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+    	    	int target=Integer.parseInt(br.readLine());
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution g = new Solution();
+        	    long ans=g.verticallyDownBST(root,target);
+        	    out.println(ans);
+                t--;
+            
+        }
+        out.close();
+    }
+  
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    long verticallyDownBST(Node root,int target)
+    {
+        Node f = find(root,target);
+        if(f==null) return -1;
+        return f(f,0)-f.data;
+    }
+    long f(Node root,int val) {
+        if(root==null) return 0;
+        long ans =0;
+        ans+=f(root.left,val-1);
+        ans+=f(root.right,val+1);
+        if(val==0) {
+            ans+=root.data;
+        }
+        return ans;
+    }
+    
+    Node find(Node root,int target) {
+        if(root==null) return null;
+        if(root.data<target) {
+            return find(root.right,target);
+        } else if(root.data>target) {
+            return find(root.left,target);
+        } else {
+            return root;
+        }
+    }
+}
