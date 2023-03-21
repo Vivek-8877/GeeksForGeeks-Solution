771862fcf64794126ecbd97466591afcc765c985
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2023-03-21 08:00:04 +0530

    Added Solution - GfG to GitHub

---
+++ March 2023/Day 21/Taxi Booking/taxi-booking.py
@@ -0,0 +1,51 @@
+from typing import List
+import sys
+
+class Solution:
+    # Time Complexity :- O(n);
+    # Space Complexity :- O(1);
+    def minimumTime(self, N : int, cur : int, pos : List[int], time : List[int]) -> int:
+        # code here
+        ans=sys.maxsize;
+        for i in range(0,N) : ans = min(ans,abs(cur-pos[i])*time[i]);
+        return ans;
+        
+
+
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        N = int(input())
+        
+        
+        cur = int(input())
+        
+        
+        pos=IntArray().Input(N)
+        
+        
+        time=IntArray().Input(N)
+        
+        obj = Solution()
+        res = obj.minimumTime(N, cur, pos, time)
+        
+        print(res)
+        
+
+# } Driver Code Ends
