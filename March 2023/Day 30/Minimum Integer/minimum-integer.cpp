1f27b7ed41ac828aa95de1992f6042d0e818fdbe
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2023-03-30 09:51:23 +0530

    Added Solution - GfG to GitHub

---
+++ March 2023/Day 30/Minimum Integer/minimum-integer.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+#define ll long long int
+class Solution {
+  public:
+    int minimumInteger(int N, vector<int> &A) {
+        // code here
+        ll sum=0;
+        for(ll a : A) sum+=a;
+        int mn=INT_MAX;
+        for(ll a : A) if(sum<=N*a) mn = min(mn,(int)a);
+        return mn;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        vector<int> A(N);
+        Array::input(A,N);
+        
+        Solution obj;
+        int res = obj.minimumInteger(N, A);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
