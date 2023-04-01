168e486d58018a756bb7cf1a10baf9a35da9604f
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2023-04-01 07:28:51 +0530

    Added Solution - GfG to GitHub

---
+++ April 2023/Day 01/Make Array Elements Equal/make-array-elements-equal.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    // Time Complexity :- O(1);
+    // Space Compleixty :- O(1);
+    long long int minOperations(long long int n) {
+        // Code here
+        return (n/2) * (n - (n/2));
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.minOperations(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
